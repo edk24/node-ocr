85ae9aeec8a93c4663f61666d5877b0db337874a
余小波 <yuxiaobo64@gmail.com>
2023-05-16 16:56:49 +0800

    跨域问题

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const formidable = require("formidable");
 const express = require('express')
 const app = express()
 
+// 跨域
+const cors = require('cors');
+app.use(cors());
+
 // ocr 配置
 const ocrConfig = {
     lang: "chi_sim",
-    oem: 1,
-    psm: 3,
+    // oem: 1,
+    // psm: 3,
 }
 
 // 首页
@@ -34,6 +38,25 @@ app.post('/api/ocr', (req, res) => {
             })
     });
 })
+
+// 图像识别ex
+// app.post('/api/ocr', (req, res) => {
+//     var form = new formidable.IncomingForm();
+//     form.parse(req, function (error, fields, files) {
+//         if (!files.img) {
+//             res.send({code:400, error:'没有识别对象'})
+//         }
+//         const img = fs.readFileSync(files.img.filepath)
+//         tesseract
+//             .recognize(img, ocrConfig)
+//             .then((text) => {
+//                 res.send({code:200, result: text})
+//             })
+//             .catch((error) => {
+//                 res.send({code:400, error: error.message})
+//             })
+//     });
+// })
 
 
 const server = app.listen(3000, function () {
